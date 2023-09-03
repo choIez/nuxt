79451c47bd97c070168c51d0710fba271a7b61af
ULannt <galaxy.nebula2021@gmail.com>
2023-09-03 21:42:42 +0800

    Commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // 给所有scss文件注入一段代码
+          additionalData: `@import '~/assets/css/variables.scss';`
+        }
+      }
+    }
+  },
+  modules: ["@element-plus/nuxt", "nuxt-swiper"]
 })
